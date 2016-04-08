9e84abc23d69f41bf2747a8dba0076ba9fdd4a9d
Divyavailissery <divyavailissery@gmail.com>
2016-04-08 10:17:08 +0530

    Update super.php

--- super.php
+++ super.php
@@ -262,15 +262,54 @@ while($row = mysqli_fetch_array($result))
   echo "</td><td>";
   echo $row['submission_date'];
   echo "</td> <td>";
+  $status=$row['status'];
+  if($status<20){
    echo <<<EOT
    <td>
-   <form  action= 'status.php' method= 'post'>
-      <select id="status" name="status">
-        <option type="text" value="progressing">progressing</option>
-        <option type="text" value="complete">complete</option>
-        <option type="text" value="incomplete">incomplete</option>
-         </select>
-  </form>
+    <div class="progress">
+                <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                  <span class="sr-only">15% Complete</span>
+                </div>
+              </div>
+  </td>
+</tr>
+
+EOT;
+}
+elseif($status>20&&$status<50){
+  echo <<<EOT
+   <td>
+    <div class="progress">
+                <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                  <span class="sr-only">40% Complete</span>
+                </div>
+              </div>
+  </td>
+</tr>
+
+EOT;
+}
+elseif($status>50&&$status<90){
+  echo <<<EOT
+   <td>
+    <div class="progress">
+                <div class="progress-bar progress-bar-aqua" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                  <span class="sr-only">80% Complete</span>
+                </div>
+              </div>
+  </td>
+</tr>
+
+EOT;
+}
+elseif($status>90){
+  echo <<<EOT
+   <td>
+    <div class="progress">
+                <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                  <span class="sr-only">95% Complete</span>
+                </div>
+              </div>
   </td>
 </tr>
 
